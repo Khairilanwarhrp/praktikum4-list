a08d8c1138865327384b44062742613c3fc06c1f
Khairilanwarhrp <92617414+Khairilanwarhrp@users.noreply.github.com>
2021-12-02 20:44:51 +0700

    Add files via upload

---
+++ tugaslist.py
@@ -0,0 +1,7 @@
+nama = ["ari","bambang"]
+nilai = ["65","70","80","90"]
+NIM = ["1234","2345"]
+print(f"Nama :{nama[0]}",F"\nNIM = {NIM[0]}")
+print(f"Nilai Tugas = {nilai[1]}",f"\nNilai UTS = {nilai[0]}",f"\nNilai UAS = {nilai[2]} ")
+print(f"Nama :{nama[1]}",F"\nNIM = {NIM[1]}")
+print(f"Nilai Tugas = {nilai[0]}",f"\nNilai UTS = {nilai[2]}",f"\nNilai UAS = {nilai[3]} ")
